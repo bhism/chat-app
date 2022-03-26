26cbefcce770ccad34a1148bd7ecd064e768fe0a
BHISM <61289719+bhism@users.noreply.github.com>
2022-03-26 16:02:34 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,10 +6,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: FirebaseOptions(
-    apiKey: "AIzaSyDuKu19lG-SLfJCFOZyehfMl8dDyjQmCi0",
-    appId: "1:1001590694791:web:0d2651bf5f7e60fbaf2922",
-    messagingSenderId: "1001590694791",
-    projectId: "chatapp11-cbb62",
+    apiKey: "",
+    appId: "",
+    messagingSenderId: "",
+    projectId: "",
   ));
   runApp(MyApp());
 }
